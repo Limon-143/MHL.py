ab5e81ff88117e9e07360e6a54ad2eefa255417b
MAHAMUD HASAN LIMON <114866115+Limon-143@users.noreply.github.com>
2022-12-17 16:43:46 +0600

    Update MHL-.py

--- MHL-.py
+++ MHL-.py
@@ -1,7 +1,7 @@
 
 from os import path
 import os,base64,zlib,pip,urllib
-os.system('xdg-open https://github.com/Limon-143)
+os.system('xdg-open https://www.facebook.com/sk.limon.tera.bap)
 print('\n\033[1;37m install modules...\n It will take some seconds...')
 
 try:
